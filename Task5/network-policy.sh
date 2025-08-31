fcaa12bb546a52b150934275452688119610da50
Artem Ufimtsev <kelwar@elbrus.ru>
2025-08-31 12:06:57 +0700

    Task5

--- Task5/network-policy.sh
+++ Task5/network-policy.sh
@@ -1,7 +1,7 @@
-kubectl run back-end-api-app --image=nginx --labels role=back-end-api --expose --port 8000
-kubectl run front-end-app --image=nginx --labels role=front-end --expose --port 8080
-kubectl run admin-back-end-api-app --image=nginx --labels role=admin-back-end-api --expose --port 8001
-kubectl run admin-front-end-app --image=nginx --labels role=admin-front-end --expose --port 8081
+kubectl run back-end-api-app --image=nginx --labels role=back-end-api --expose --port 8001
+kubectl run front-end-app --image=nginx --labels role=front-end --expose --port 8081
+kubectl run admin-back-end-api-app --image=nginx --labels role=admin-back-end-api --expose --port 8002
+kubectl run admin-front-end-app --image=nginx --labels role=admin-front-end --expose --port 8082
 
 kubectl apply -f non_admin_api_allow.yaml
 kubectl apply -f non_admin_frontend_allow.yaml
