f220f2d8df81d66a0e2f408617e55a8c4201bdce
Artem Ufimtsev <kelwar@elbrus.ru>
2025-08-31 10:43:34 +0700

    Task4

---
+++ Task4/users.sh
@@ -0,0 +1,7 @@
+kubectl apply -f users/manager.yaml
+kubectl apply -f users/support.yaml
+kubectl apply -f users/accountant.yaml
+kubectl apply -f users/analytic.yaml
+kubectl apply -f users/bi.yaml
+kubectl apply -f users/owner.yaml
+kubectl apply -f users/devops.yaml
